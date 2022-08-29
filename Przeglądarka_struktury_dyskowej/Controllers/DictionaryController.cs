0392bbf8dd0b9a62e20b6f0f6c26214a2d353c1e
AdamRat <adam.ratajczak994@gmail.com>
2022-08-29 21:40:12 +0200

    Add Data to read file and dictionary and make controller to first tests

---
+++ Przeglądarka_struktury_dyskowej/Controllers/DictionaryController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Przeglądarka_struktury_dyskowej.Models;
+using Microsoft.Extensions.Options;
+
+namespace Przeglądarka_struktury_dyskowej.Controllers
+{
+    [ApiController]
+    [Route("Data")]
+    public class DictionaryController : Controller
+    {
+        
+        private Data data = new Data();
+        // GET: DictionaryController
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return data.GetValues("");
+        }
+        //// GET: DictionaryController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: DictionaryController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: DictionaryController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: DictionaryController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: DictionaryController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: DictionaryController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: DictionaryController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ Przeglądarka_struktury_dyskowej/Models/Data.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Przeglądarka_struktury_dyskowej.Models
+{
+
+    public class Data
+    {
+        
+        public List<string> GetValues(string path)
+        {
+            string DirName = @"C:\Users\rataj\Desktop\exe";
+            string[] files =Directory.GetFiles(DirName+path);
+            string[] dir=Directory.GetDirectories(DirName+path);
+            return Makelist(files, dir);
+        }
+        private List<string> Makelist(string[] fiArr, string[] dir)
+        {
+            List<string> filesInformation = new List<string>();
+            foreach (string s in dir)
+            {
+                FileInfo di = new FileInfo(s);
+                filesInformation.Add(GetDirInfo(di));
+            }
+            foreach (string s in fiArr)
+            {
+                FileInfo fi = new FileInfo(s);
+                filesInformation.Add(GetFileInfo(fi));
+            }
+            return filesInformation;
+        }
+        private string GetFileInfo(FileInfo fi)
+        {
+            string info = (
+                fi.Extension + "*" +
+                fi.Name + "*" +
+                fi.LastWriteTime.ToString() + "*" +
+                fi.Length.ToString() + "*" +
+                fi.Attributes.ToString());
+            return info;
+        }
+        private string GetDirInfo(FileInfo fi)
+        {
+            string info = (
+                fi.Extension + "*" +
+                fi.Name + "*" +
+                fi.LastWriteTime.ToString() + "*" +
+                fi.Attributes.ToString());
+            return info;
+        }
+    }
+}
